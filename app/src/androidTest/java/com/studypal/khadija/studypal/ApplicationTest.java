18c01e44fbea9463720d163ac35febc1721f5604
KhadijaDaruwala <khadija.genius@gmail.com>
2016-01-10 12:28:20 +0530

    Project setup

---
+++ app/src/androidTest/java/com/studypal/khadija/studypal/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.studypal.khadija.studypal;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
